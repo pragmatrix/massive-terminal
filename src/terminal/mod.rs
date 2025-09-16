ee1e8f59e52bf9aacf39e93fdc83871cdc596d9e
Armin Sander <armin@replicator.org>
2025-09-16 13:08:15 +0200

    fmt

--- src/terminal/mod.rs
+++ src/terminal/mod.rs
@@ -1,11 +1,11 @@
 mod font;
 mod geometry;
-mod view;
 mod scroller;
 mod state;
+mod view;
 
 pub use font::*;
 pub use geometry::*;
-pub use view::*;
 pub use scroller::*;
 pub use state::*;
+pub use view::*;
