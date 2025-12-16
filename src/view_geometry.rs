5131322abe34462b8a229cf2511b0dc6ab193750
Armin Sander <armin@replicator.org>
2025-12-16 07:31:02 +0100

    Prevent confusion about the inner size of a view

--- src/view_geometry.rs
+++ src/view_geometry.rs
@@ -14,7 +14,7 @@ pub type PixelPoint = euclid::Point2D<f64, PixelUnit>;
 #[derive(Debug)]
 pub struct ViewGeometry {
     _scale_factor: f64,
-    inner_size_px: SizePx,
+    size: SizePx,
 
     /// Padding around the terminal in physical pixels.
     padding_px: u32,
@@ -26,25 +26,25 @@ impl ViewGeometry {
         scale_factor: f64,
         padding_px: u32,
     ) -> Self {
-        let size = terminal_geometry.size_px();
-        let inner_size_px = size + SizePx::new(padding_px * 2, padding_px * 2);
+        let terminal_size = terminal_geometry.size_px();
+        let size_px = terminal_size + SizePx::new(padding_px * 2, padding_px * 2);
 
         Self {
             _scale_factor: scale_factor,
-            inner_size_px,
+            size: size_px,
             padding_px,
         }
     }
 
     pub fn inner_size_px(&self) -> SizePx {
-        self.inner_size_px
+        self.size
     }
 
-    /// Returns the terminal inner size in pixel.
-    pub fn resize(&mut self, new_inner_size_px: SizePx) -> SizePx {
+    /// Returns the terminal size in pixel.
+    pub fn resize(&mut self, size_px: SizePx) -> SizePx {
         let padding_2 = self.padding_px * 2;
-        let terminal_inner_size = new_inner_size_px.saturating_sub((padding_2, padding_2).into());
-        self.inner_size_px = new_inner_size_px;
+        let terminal_inner_size = size_px.saturating_sub((padding_2, padding_2).into());
+        self.size = size_px;
         terminal_inner_size
     }
 }
